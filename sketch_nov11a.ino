939c3aaff58b5b62bab4ca713b778c707f95d904
Manuel Navarrete <89426044+manunava11@users.noreply.github.com>
2024-11-11 20:11:04 -0300

    Update sketch_nov11a.ino

--- sketch_nov11a.ino
+++ sketch_nov11a.ino
@@ -1,9 +1,13 @@
 const int pinEntrada = 34;        // Pin analógico donde se mide la señal (Ejemplo: GPIO34)
 const int pinSalida = 13;         // Pin digital que se activará en el cruce por cero
-
+int alfa = 0;
 const float umbralCero = 2.5;     // Umbral para el cruce por cero (ajustar si es necesario)
 const int pinesSalida[] = {13,14,15,16,17,18,19};
 int i=0;
+int lim_alfa = 90;
+const int pinAlfa = ;
+const int pinAlfaLectura = ;
+const int 
 void setup() {
     pinMode(pinSalida, OUTPUT);
     Serial.begin(115200);
@@ -17,7 +21,7 @@ void setup() {
 
 void loop() {
     detectarCrucePorCero(100);             // Activar el pin de salida por 500 ms en cada cruce por cero
-  
+    medirAlfa();
 }
 
 // Función para generar una señal de onda sinusoidal aproximada en el pin PWM
@@ -93,4 +97,11 @@ float medirFrecuencia() {
         }
     }
    return 0;
+}
+
+void medirAlfa(){
+  digitalWrite(pinAlfa, HIGH);
+  float vo = analogRead(pinAlfaLectura)* (3.3 / 4095.0);
+  alfa = lim_alfa-(lim_alfa/3.3)*vo
+  digitalWrite(pinAlfa, LOW);
 }
